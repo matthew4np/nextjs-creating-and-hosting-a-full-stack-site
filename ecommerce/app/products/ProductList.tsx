7c69a51a08b627d463f6c6eaefab9e3b79bd8cfd
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-06-25 16:39:47 +0000

    Route Paramters

--- ecommerce/app/products/ProductList.tsx
+++ ecommerce/app/products/ProductList.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
-import { Product } from "./product-data";
+import Image from 'next/image';
+import { Product } from './product-data';
+import Link from 'next/link';
 
 export default function ProductList({ products } : { products: Product[]}) {
     return (
         <div>
             {products.map(product => (
-                <div key ={product.id}>
+                <Link key ={product.id} href={"/products/" + product.id}>
                     <Image src={'/' + product.imageUrl} alt="Product image" width={150} height={150} />
                     <h2>{product.name}</h2>
                     <p>${product.price}</p>
-                    </div>
+                </Link>
             ))}
         </div>
     )
