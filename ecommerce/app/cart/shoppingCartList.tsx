566baa6ea68cbe3200c7f99cdc1905c859b28d1b
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-06-28 10:52:54 +0000

    04-6

---
+++ ecommerce/app/cart/shoppingCartList.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from "react";
+import { Product } from "../products/product-data";
+import Link from 'next/link'
+
+export default function ShoppingCartList({ initialCartProducts } : {initialCartProducts: Product[] }) {
+
+    const [cartProducts] = useState( initialCartProducts );
+
+    return (
+    <>
+    <h1>Shopping Cart</h1>
+    {cartProducts.map(product => (
+        <Link key={product.id} href={"/products/" + product.id}>
+            <h3>{product.name}</h3>
+            <p>${product.price}</p>
+        </Link>
+    ))}
+    </>
+    )
+}
