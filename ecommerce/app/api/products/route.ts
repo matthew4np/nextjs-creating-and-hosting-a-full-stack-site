b2392738aaff51be267391dc091339d933f7051b
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-06-28 05:09:13 +0000

    02-5

--- ecommerce/app/api/products/route.ts
+++ ecommerce/app/api/products/route.ts
@@ -1,6 +1,11 @@
-import {products} from '@/app/products/product-data';
+import { connectToDb} from '../db';
 
 export async function GET() {
+
+    const { db } = await connectToDb();
+
+
+
     return new Response(JSON.stringify(products), {
         status: 200,
         headers: {
