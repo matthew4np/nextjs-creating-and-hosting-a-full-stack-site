552a5a9690a4052c3b15102bb1fcbbba1db251dc
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-06-30 14:50:47 +0000

    30 June 2025 1050PM

--- ecommerce/app/fetchtest/page.tsx
+++ ecommerce/app/fetchtest/page.tsx
@@ -1,6 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 export default async function FetchTest() {
 
-    const response = await fetch('http://127.0.0.1:3000/api/hello');
+    const response = await fetch('process.env.NEXT_PUBLIC_SITE_URL/api/hello');
     const data = await response.json();
     
     return <h1>{data.message}</h1>
