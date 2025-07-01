26f5fa18f1f6fed6d389bde043c0d33ade5dc119
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-07-01 15:41:23 +0000

    1 July 2025 1141PM

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic';
 
 export default async function CartPage() {
 
-    const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/users/2/cart', {
+    const response = await fetch('https://solid-happiness-x5r7xwrg6vjxh6p77-3000.app.github.dev/api/users/2/cart', {
         cache: 'no-cache'
     }
     );
