0006b629131568139cf37a6d6e946e2c3b04a3ff
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2025-07-19 15:18:16 +0800

    19 July 2025 318PM

--- ecommerce/app/users/page.tsx
+++ ecommerce/app/users/page.tsx
@@ -10,6 +10,7 @@ export default async function Users() {
     const response2 = await fetch('http://localhost:3000/api/users/2/cart', {
         cache : 'no-cache',
     })
+
     const listUsers = await response2.json();
 
     return (
