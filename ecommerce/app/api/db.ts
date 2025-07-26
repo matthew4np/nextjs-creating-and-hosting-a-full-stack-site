c447c8ac3fd4cd2d82503dd7d9a4a28208cb52b3
DipSinYaw <ICT_3337922K@connect.np.edu.sg>
2025-07-26 14:23:43 +0800

    modify mongodb template

--- ecommerce/app/api/db.ts
+++ ecommerce/app/api/db.ts
@@ -10,7 +10,8 @@ if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb};
 }
 
-  const uri = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/?retryWrites=true&w=majority&appName=Cluster0`;
+  
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
 serverApi: {
